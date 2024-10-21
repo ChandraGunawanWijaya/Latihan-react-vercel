24d5b8de3a1312f5c418229fcae57d48eb93c13d
ChandraGunawanWijaya <161033234+ChandraGunawanWijaya@users.noreply.github.com>
2024-10-21 20:02:41 +0700

    chann

---
+++ my-react/build/static/js/900.f24775bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_react=self.webpackChunkmy_react||[]).push([[900],{900:(n,e,i)=>{i.r(e),i.d(e,{default:()=>t});i(43);var a=i(579);function s(n){const{nama:e,pesan:i}=n;return(0,a.jsxs)("h1",{children:["Hello ",e," , ",i]})}s.defaultProps={nama:"Chindri Giniwin",pesan:"Silimit diting di PW2 IF31"};const t=s}}]);
+//# sourceMappingURL=900.f24775bb.chunk.js.map
--- my-react/src/route.js
+++ my-react/src/route.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Home = React.lazy(() => import("./Hello"));
 const product = React.lazy(() => import("./Product"));
-const Register = React.lazy(() => import("./Registrasi"));
+const register = React.lazy(() => import("./Registrasi"));
 
 const routes = [
     {path : "/" , Component : Home},
     {path : "/Product" , Component : product},
-    {path : "/Registrasi" , Component : Register},
+    {path : "/Registrasi" , Component : register},
 ];
 
 export default routes;
